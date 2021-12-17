b3e65db5c8879f91b80af33b1496180cb394f5e0
MrArKi <925614745@qq.com>
2021-12-17 22:47:18 +0800

    logback使用异步打印

--- logback/src/main/java/com/zp/LogbackTest.java
+++ logback/src/main/java/com/zp/LogbackTest.java
@@ -44,10 +44,20 @@ public class LogbackTest {
     @Test
     public void test03() {
         Logger logger = LoggerFactory.getLogger(this.getClass());
-        logger.error("error信息");
-        logger.warn("warn信息");
-        logger.info("info信息");
-        logger.debug("debug信息");
-        logger.trace("trace信息");
+
+        for (int i = 0; i < 1000; i++) {
+            logger.error("error信息");
+            logger.warn("warn信息");
+            logger.info("info信息");
+            logger.debug("debug信息");
+            logger.trace("trace信息");
+        }
+
+        System.out.println("11111111111111");
+        System.out.println("22222222222222");
+        System.out.println("33333333333333");
+        System.out.println("44444444444444");
+        System.out.println("55555555555555");
+        System.out.println("66666666666666");
     }
 }
