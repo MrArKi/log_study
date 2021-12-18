707d2a072d88bb70e6262c19adf78ad5d438da63
MrArKi <925614745@qq.com>
2021-12-18 19:45:12 +0800

    log4j2配合slf4j

--- log4j2/src/main/java/com/zp/Test01.java
+++ log4j2/src/main/java/com/zp/Test01.java
@@ -1,8 +1,8 @@
 package com.zp;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TODO
@@ -16,10 +16,21 @@ public class Test01 {
      * 入门
      * 默认error日志级别
      */
+//    @Test
+//    public void test01() {
+//        Logger logger = LogManager.getLogger(this.getClass());
+//        logger.fatal("fatal信息");
+//        logger.error("error信息");
+//        logger.warn("warn信息");
+//        logger.info("info信息");
+//        logger.debug("debug信息");
+//        logger.trace("trace信息");
+//    }
+
     @Test
-    public void test01() {
-        Logger logger = LogManager.getLogger(this.getClass());
-        logger.fatal("fatal信息");
+    public void test02() {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
         logger.error("error信息");
         logger.warn("warn信息");
         logger.info("info信息");
